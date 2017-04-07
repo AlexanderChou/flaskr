9d6db5c87b9b9317ab529cdc3ce6c2cabac43fba
AlexanderChou <290573449@qq.com>
2017-04-07 16:12:04 +0800

    update
    updated

--- flaskr.py
+++ flaskr.py
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 import os
 import sqlite3
+import newip
 from  flask import  Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
@@ -44,7 +44,11 @@ def init_db():
 def query():
     if request.method == 'GET':
         return render_template("show_query.html",results=[])
-
+    else:
+        url = request.form['url']
+        results = newip.cloak(url)
+        results = set(results)
+        return render_template("show_query.html", results=results)
 @app.route('/show')
 def show_entries():
     db = get_db()
@@ -96,103 +100,4 @@ def logout():
     return redirect(url_for('show_entries'))
 
 if __name__ == '__main__':
-=======
-import os
-import sqlite3
-from  flask import  Flask, request, session, g, redirect, url_for, abort, render_template, flash
-
-app = Flask(__name__)
-
-
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
-    DEBUG=True,
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-
-app.config.from_envvar("FLASKR_SETTINGS", silent=True)
-
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            temp = f.read()
-            db.cursor().executescript(temp)
-        db.commit()
-
-
-@app.route('/',methods=['POST', 'GET'])
-def query():
-    if request.method == 'GET':
-        return render_template("show_query.html",results=[])
-
-@app.route('/show')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
-    return render_template("show_entries.html", entries=entries)
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
-
-@app.route('/del', methods=['POST'])
-def del_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('delete from entries where title=?', [request.form['title']])
-    db.commit()
-    flash("The entry has been deleted!")
-    return redirect(url_for('show_entries'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if session.get('logged_in'):
-        flash("You have already logged in!")
-        return redirect(url_for('show_entries'))
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in.')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out.')
-    return redirect(url_for('show_entries'))
-
-if __name__ == '__main__':
->>>>>>> origin/master
     app.run()
--- schema.sql
+++ schema.sql
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 DROP TABLE if exists entries;
 create table entries (
   id integer PRIMARY KEY autoincrement,
   title string not null,
   text string not null
-=======
-DROP TABLE if exists entries;
-create table entries (
-  id integer PRIMARY KEY autoincrement,
-  title string not null,
-  text string not null
->>>>>>> origin/master
 );
